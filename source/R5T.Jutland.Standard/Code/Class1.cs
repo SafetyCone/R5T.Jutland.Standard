eacbfe573f350c867cf742a930c3c6eebce119ce
MinexAutomation <minex.automation@gmail.com>
2020-02-17 16:10:41 -0800

    Added IServiceCollection extensions.

--- source/R5T.Jutland.Standard/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Jutland.Standard
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Jutland.Standard/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+using R5T.Jutland.Newtonsoft;
+
+
+namespace R5T.Jutland.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="IJsonFileSerializationOperator"/> service.
+        /// </summary>
+        public static IServiceCollection AddJsonFileSerializationOperator(this IServiceCollection services)
+        {
+            services.AddNewtonsoftJsonFileSerializationOperator();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IJsonFileSerializationOperator"/> service.
+        /// </summary>
+        public static ServiceAction<IJsonFileSerializationOperator> AddJsonFileSerializationOperatorAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IJsonFileSerializationOperator>(() => services.AddJsonFileSerializationOperator());
+            return serviceAction;
+        }
+    }
+}
